fa4cb4966beebc382bf7be1005f4cb7ebfa20648
Cristobal Valenzuela <cris@runwayml.com>
2019-07-21 16:20:30 -0400

    add more checkpoint options

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Progressive Growing of GANs (PGAN) ported to Runway
+
+<a href="http://sdk.runwayml.com" target="_blank"><img src="https://runway.nyc3.cdn.digitaloceanspaces.com/assets/github/runway-badge.png" width=100/></a>
+
+This repository contains a version of the Python Progressive Growing of GANs (PGAN) model ported to Runway. Check the [Runway Documentation](http://docs.runwayml.com) for a tutorial demonstrating this model porting process.
--- runway_model.py
+++ runway_model.py
@@ -3,10 +3,8 @@ import torchvision
 import numpy as np
 import runway
 
-checkpoint_description = "Pretrained checkpoints to use."
-
-@runway.setup(options={"checkpoint": runway.category(description=checkpoint_description,
-                                                choices=['celebAHQ-512', 'celebAHQ-256', 'DTD', 'celeba'],
+@runway.setup(options={"checkpoint": runway.category(description="Pretrained checkpoints to use.",
+                                                choices=['celebAHQ-512', 'celebAHQ-256', 'celeba'],
                                                 default='celebAHQ-512')})
 def setup(opts):
     checkpoint = opts['checkpoint']
@@ -15,13 +13,12 @@ def setup(opts):
     return model
 
 @runway.command('generate',
-                inputs={ 'z': runway.vector(512, sampling_std=0.5)},
+                inputs={ 'z': runway.vector(length=512, sampling_std=0.5)},
                 outputs={ 'image': runway.image })
 def generate(model, inputs):
     z = inputs['z']
     latents = z.reshape((1, 512))
     latents = torch.from_numpy(latents)
-    
     with torch.no_grad():
         generated_image = model.test(latents.float())
     generated_image = generated_image.clamp(min=-1, max=1)
