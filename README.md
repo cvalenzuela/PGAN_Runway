9113cd079089a0139788a016cbfe25057eef18d0
Cristobal Valenzuela <cris@runwayml.com>
2019-07-30 16:35:29 -0400

    some clean up :ok_hand:

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 <a href="http://sdk.runwayml.com" target="_blank"><img src="https://runway.nyc3.cdn.digitaloceanspaces.com/assets/github/runway-badge.png" width=100/></a>
 
-This repository contains a version of the Python Progressive Growing of GANs (PGAN) model ported to Runway. Check the [Runway Documentation](http://docs.runwayml.com) for a tutorial demonstrating this model porting process.
+This repository contains a version of the Python [Progressive Growing of GANs (PGAN) model](https://github.com/tkarras/progressive_growing_of_gans) ported to Runway. 
+
+Check out the blog post following this example code:
+
+https://medium.com/runwayml/porting-a-machine-learning-model-from-github-to-runway-in-5-minutes-555c5c9310af
+
+## Useful Links:
+
+- [Runway Model Template](https://github.com/runwayml/model-template)
+- [Runway Documentation](http://docs.runwayml.com)
+- [Video Tutorials](https://www.youtube.com/channel/UCUBqu_z5uP0AZhYtuyFZB3g)
--- runway_model.py
+++ runway_model.py
@@ -1,10 +1,25 @@
+## -----
+# Sample code to port a model to Runway
+# Check out the blog post to learn more
+# https://medium.com/runwayml/porting-a-machine-learning-model-from-github-to-runway-in-5-minutes-555c5c9310af
+#
+#
+# Runway Python SDK: https://sdk.runwayml.com/en/latest/
+## -----
+
+# Import our dependencies
 import torch
 import torchvision
 import numpy as np
-import runway
+import runway # Be sure to install it first!: pip3 install runway-python
 
+# Setup the model, initialize weights, set the configs of the model, etc.
+# Every model will have a different set of configurations and requirements.
+# Check https://sdk.runwayml.com/en/latest/runway_module.html to see a complete
+# list of supported configs. The setup function should return the model ready to
+# be used.
 @runway.setup(options={"checkpoint": runway.category(description="Pretrained checkpoints to use.",
-                                                choices=['celebAHQ-512', 'celebAHQ-256', 'celeba'],
+                                                choices=['celebAHQ-512', 'celebAHQ-256', 'celeba', 'DTD'],
                                                 default='celebAHQ-512')})
 def setup(opts):
     checkpoint = opts['checkpoint']
@@ -12,6 +27,9 @@ def setup(opts):
     model = torch.hub.load('facebookresearch/pytorch_GAN_zoo:hub', 'PGAN', model_name=checkpoint, pretrained=True, useGPU=use_gpu)
     return model
 
+# Every model needs to have at least one command. Every command allows to send
+# inputs and process outputs. To see a complete list of supported inputs and
+# outputs data types: https://sdk.runwayml.com/en/latest/data_types.html
 @runway.command('generate',
                 inputs={ 'z': runway.vector(length=512, sampling_std=0.5)},
                 outputs={ 'image': runway.image })
@@ -25,5 +43,6 @@ def generate(model, inputs):
     generated_image = ((generated_image + 1.0) * 255 / 2.0)
     return generated_image[0].permute(1, 2, 0).numpy().astype(np.uint8)
 
+# Run the model
 if __name__ == '__main__':
     runway.run(port=5232)
